4c4500d2c368a8c6c2fc23962881f2138b5d2585
Arun <149802187+Mr-Phantomm@users.noreply.github.com>
2026-04-20 17:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.13%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -10,7 +10,21 @@ class Solution {
 
     }
 
+    public int tabuLation(int m,int n){
+        int[][] dp = new int[m+1][n+1];
+        dp[1][1]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                // System.out.println("Ran for"+i+" "+j);
+                if(i!=m)dp[i+1][j]+=dp[i][j];
+                if(j!=n)dp[i][j+1]+=dp[i][j];
+            }
+        }
+        return dp[m][n];
+    }
+
     public int uniquePaths(int m, int n) {
-        return dp(m,n,new Integer[m+1][n+1]);
+        // return dp(m,n,new Integer[m+1][n+1]);/
+        return tabuLation(m,n);
     }
 }
