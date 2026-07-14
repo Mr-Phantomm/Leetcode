d3641e8696ea680183e1dd3c0c55cd5afdc5a711
Arun <149802187+Mr-Phantomm@users.noreply.github.com>
2026-07-14 22:35:23 +0530

    Time: 467 ms (5.20%) | Memory: 47.1 MB (7.15%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,55 +1,77 @@
 class Solution {
-    public boolean solve(char[][] board) {
 
-        for (int i = 0; i < 9; i++) {
+    public boolean solve(int i,int j,char[][] board,Map<Integer,Set<Character>> row,Map<Integer,Set<Character>> column,Map<Integer,Set<Character>> box){
+        if(i==9)return true;
+        int nextI=(j==8)?i+1:i;
+        int nextJ=(j==8)?0:j+1;
 
-            for (int j = 0; j < 9; j++) {
+        if(board[i][j]!='.'){
+            return solve(nextI,nextJ,board,row,column,box);
+        }   
 
-                if (board[i][j] == '.') {
 
-                    for (char num = '1'; num <= '9'; num++) {
+        int boxNumber=(i/3)*3+(j/3);
+        Set<Character> currRow=row.get(i);
+        Set<Character> currCol=column.get(j);
+        Set<Character> currBox=box.get(boxNumber);
 
-                        if (isValid(board, i, j, num)) {
-
-                            board[i][j] = num;
-
-                            if (solve(board))
-                                return true;
-
-                            board[i][j] = '.';
-                        }
-                    }
-
-                    return false;
-                }
+        for(char ch='1';ch<='9';ch++){
+            if(!currRow.contains(ch)&&!currCol.contains(ch)&&!currBox.contains(ch)){
+                board[i][j]=ch;
+                currRow.add(ch);
+                currCol.add(ch);
+                currBox.add(ch);
+                if(solve(nextI,nextJ,board,row,column,box))return true;
+                board[i][j]='.';
+                currRow.remove(ch);
+                currCol.remove(ch);
+                currBox.remove(ch);
             }
         }
-
-        return true;
-    }
-
-    public boolean isValid(char[][] board, int row, int col, char num) {
-
-        for (int i = 0; i < 9; i++) {
-
-            if (board[row][i] == num)
-                return false;
-
-            if (board[i][col] == num)
-                return false;
-
-            int newRow = 3 * (row / 3) + i / 3;
-            int newCol = 3 * (col / 3) + i % 3;
-
-            if (board[newRow][newCol] == num)
-                return false;
-        }
-
-        return true;
+        return false;
     }
 
     public void solveSudoku(char[][] board) {
-        solve(board);
+        
+        // Data Structure Initialize
+        
+        Map<Integer,Set<Character>> row = new HashMap<>();
+        Map<Integer,Set<Character>> column=new HashMap<>();
+        Map<Integer,Set<Character>> box=new HashMap<>();
+        
+        // Data Structure filler 
+        
+        for(int i=0;i<9;i++){
+            row.put(i,new HashSet<>());
+            column.put(i,new HashSet<>());
+            box.put(i,new HashSet<>());
+        } 
+        
+        //Traversal and DATA INPUT   
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')continue;
+                row.get(i).add(board[i][j]);
+                column.get(j).add(board[i][j]);
+                int boxIndex=(i/3)*3+(j/3);
+                box.get(boxIndex).add(board[i][j]);                  
+            }
+        }
+        
+        solve(0,0,board,row,column,box);
+        
+
+        
+        
+        // int boxIndex=
+        // ["5","3",".",".","7",".",".",".","."],
+        // ["6",".",".","1","9","5",".",".","."],
+        // [".","9","8",".",".",".",".","6","."],
+        // ["8",".",".",".","6",".",".",".","3"],
+        // ["4",".",".","8",".","3",".",".","1"],
+        // ["7",".",".",".","2",".",".",".","6"],
+        // [".","6",".",".",".",".","2","8","."],
+        // [".",".",".","4","1","9",".",".","5"],
+        // [".",".",".",".","8",".",".","7","9"],
     }
-    
 }
